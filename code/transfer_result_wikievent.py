d5d520820d34f18efb4c7cf6e48dffc9c20a14ad
doublecolor <kym990708@gm.gist.ac.kr>
2024-11-12 10:52:04 +0900

    init

---
+++ code/transfer_result_wikievent.py
@@ -0,0 +1,15 @@
+
+
+# ontology는 wikievent ontology를 사용한다.
+# span, multi-event, coref 정보를 모두 포함함.
+
+'''
+IN:
+  - golden span format
+  - predictions span format
+OUT:
+  - golden head format
+  - predictions head format
+DESCRTIPION:
+  - For Head F1 score, we need to convert the span format to head format
+'''
